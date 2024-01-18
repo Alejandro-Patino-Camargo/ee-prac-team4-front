7b4f69d6bcbd48599e1800d3edbbe7fd86a9a1c9
alejandro-patino-camargo <alejandropatinoc@gmail.com>
2024-01-17 21:41:01 -0800

    updates env varables

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 let base_url;
 
 process.env.NODE_ENV === "production"
-  ? (base_url = "https://skill-stacks-backend.onrender.com")
+  ? (base_url = process.env.REACT_APP_API_URI)
   : (base_url = "http://localhost:8000");
 
 export { base_url };
